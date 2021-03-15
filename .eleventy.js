79c37d668256d77eaa9895be1bc482a770101f25
Brian Gunzenhauser <brian@boxandpixel.com>
2021-03-15 16:34:26 -0400

    Add sitemap.xml

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.setBrowserSyncConfig({
@@ -12,6 +14,13 @@ module.exports = function (eleventyConfig) {
                 });
             }
         }
+    });
+
+    eleventyConfig.addPlugin(pluginRss);
+
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
     });
 
     eleventyConfig.addPassthroughCopy("css")
