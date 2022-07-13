4b3c1fd456331b98f6280daf3120854f3793c9d3
Brian Gunzenhauser <brian@boxandpixel.com>
2022-07-13 16:27:08 -0400

    Rebuild and add thank you page again

---
+++ pages/thanks.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Layout from '../components/layout'
+import introStyles from '../styles/Intro.module.css'
+
+
+export default function Thanks() {
+    return (
+        <div>
+            <Head>
+                <title>Thank You - Brian Gunzenhauser</title>
+                <meta name="description" content="about Brian" />
+            </Head>
+
+            <Layout>
+                <h1 className={introStyles.introHeading}>Thank You. <span>I'll be in touch soon.</span></h1>
+
+            </Layout>
+        </div>
+    )
+}
+
