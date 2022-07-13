1113ad79bdacbc2ce0af4bccbf4c8e57b7ff28cf
Brian Gunzenhauser <brian@boxandpixel.com>
2022-07-13 16:38:36 -0400

    Update form attributes

--- components/form.js
+++ components/form.js
@@ -4,13 +4,14 @@ import buttonStyles from '../styles/Button.module.css'
 export default function Form() {
     return (
         <form name="contact" action="/thanks" method="POST" data-netlify="true" className={formStyles.contactForm}>
-            <label for="yourname">Your Name</label>
+            <input type="hidden" name="form-name" value="contact" />
+            <label htmlFor="yourname">Your Name</label>
             <input type="text" name="yourname" id="yourname" placeholder="Your name" />
 
-            <label for="youremail">Your Email</label>
+            <label htmlFor="youremail">Your Email</label>
             <input type="email" name="youremail" id="youremail" placeholder="Your email" />
 
-            <label for="yourmessage">Your Message</label>
+            <label htmlFor="yourmessage">Your Message</label>
             <textarea name="yourmessage" id="yourmessage" placeholder="Your message"></textarea>
 
             <button type="submit" className={buttonStyles.buttonLink}>Send it to me</button>
